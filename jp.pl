7665c89f2c736f9f6fecbb2a8771d3639e9bc6dc
Alexander Tamm <alextamm@users.noreply.github.com>
2016-05-02 12:23:14 +0300

    - Refactoring to support array input better and maintain intended functionality for hashes contained. - Added support to delete array elements.

--- jp.pl
+++ jp.pl
@@ -11,14 +11,40 @@ use warnings;
 use Storable qw(dclone);
 use Data::Diver qw(DiveRef);
 
-Getopt::Long::Configure('no_ignore_case');
-
-my $j= JSON::XS->new();
-
 my $pretty;
 my $reduce;
 my @select;
 my @delete;
+
+sub manipulate {
+    my ($ref )= @_;
+    if( ref $ref eq 'ARRAY' ) {
+        for my $item ( @$ref ) {
+            manipulate($item);
+        }
+    }
+    elsif( ref $ref eq 'HASH' ) {
+        if ( $reduce ) {
+            %$ref = %{ $ref->{$reduce} };
+        }
+
+        for my $del_key (@delete) {
+            my @path = split(/\./,$del_key);
+            my $last_key = pop @path;
+            my $deep_ref = ${ DiveRef($ref, @path) };
+            if( ref $deep_ref eq 'HASH' ) {
+                delete $deep_ref->{ $last_key };
+            }
+            elsif( ref $deep_ref eq 'ARRAY' ) {
+                if( $last_key =~ /\D/ ) {
+                    croak( join('.', @path) . " is an array, but $last_key is not a number/");
+                }
+                splice( @$deep_ref, $last_key, 1);
+            }
+        }
+    }
+}
+Getopt::Long::Configure('no_ignore_case');
 GetOptions(
     "pretty|p"   => \$pretty,
     "reduce|r=s" => \$reduce,
@@ -29,32 +55,17 @@ GetOptions(
 local $/;
 my $s=<>;
 exit unless $s;
+my $j= JSON::XS->new();
 
 my $orig = $j->decode($s);
 my $new = dclone($orig);
 my @objects;
 
-if( ref $new eq 'ARRAY' ) {
-    @objects = @$new;
-}
-elsif( ref $new eq 'HASH') {
-    push @objects, $new;
-}
-for my $h (@objects) {
-    if ( $reduce ) {
-        $h = $h->{$reduce};
-    }
+manipulate($new);
 
-    for my $del_key (@delete) {
-        my @path = split(/\./,$del_key);
-        my $last_key = pop @path;
-        my $ref = ${ DiveRef($h, @path) };
-        delete $ref->{ $last_key};
-    }
-
-    if ( $pretty ) {
-        $j->pretty(1);
-        $j->space_before(0);
-    }
+if ( $pretty ) {
+    $j->pretty(1);
+    $j->space_before(0);
 }
+
 print $j->encode($new);
